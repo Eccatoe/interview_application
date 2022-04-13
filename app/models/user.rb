e2f2a2c9222b00d104686ed728bdf61648b85680
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-04-12 20:33:15 -0400

    controller/request testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :score, numericality: true
+    def full_name
+        [first_name, last_name].join(" ")
+    end
 end
---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  let(:valid_attributes) do 
+    {
+      'id'=> 1,
+      'first_name'=> 'Aida',
+      'last_name'=> 'Buckridge',
+      'email'=> 'tambra@harber.co',
+      'city'=> 'Port Evetteborough',
+      'city'=> 'Indiana',
+      'score'=>3
+    }
+  # let(:invalid_attributes) do 
+  #   {
+  #     'id'=> 1,
+  #     'first_name'=> 'Aida',
+  #     'last_name'=> 'Buckridge',
+  #     'email'=> 'tambra@harber.co',
+  #     'city'=> 'Port Evetteborough',
+  #     'city'=> 'Indiana',
+  #     'score'=>3
+  #   }
+  end
+  describe "GET /index" do
+    it 'render all users' do
+      get users_url
+      expect(response).to have_http_status(200)
+    end
+  end
+  describe 'GET /show' do
+    it 'renders a single user' do
+    user=User.create(valid_attributes)
+    get user_path(user)
+    expect(response).to have_http_status(200)
+    end
+  end
+  describe 'GET /new' do
+    it 'renders a form to create a user' do
+    get new_user_path(user)
+    expect(response).to have_http_status(200)
+    end
+  end
+  describe 'GET /edit' do
+    it 'renders a form to edit a user' do
+    user=User.create(valid_attributes)
+    get user_path(user)
+    expect(response).to have_http_status(200)
+    end
+  end
+  describe 'PUT /update' do
+    it 'allows a user to be updated' do
+    user=User.create(valid_attributes)
+    get user_path(user)
+    expect(response).to have_http_status(200)
+    end
+  end
+  # describe 'DELETE /destroy' do
+  #   user=User.create(valid_attributes)
+  #   get user_path(user)
+  #   expect(response).to have_http_status(200)
+  # end
+end
